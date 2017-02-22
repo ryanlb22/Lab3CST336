bcc8c01cbd904b66914a42b4ee6ad68a74e71c3e
Ryan Lb <rlb.lebon@gmail.com>
2017-02-22 00:46:52 +0000

    update php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+function getHand(){
+    
+}
+function displayHand(){
+    
+}
+function displayWinners(){
+    
+}
+
+?>
+<html>
+    <header>
+    <title>Silver Jack</title>
+        <center>
+            <h1>Silver Jack</h1>
+        </center>
+    </header>
+    
+    <body>
+        <hr>
+        <footer>
+            <center>
+                Disclaimer: All material above is used for teaching purposes. Information might be inaccurate.
+            </center>
+        </footer>
+    </body>
+</html>
+
